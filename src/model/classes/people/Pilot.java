aacbed5dbdf29daee5c91e9138fbba72d2cf106b
fdobrzynski <280637@student.pwr.edu.pl>
2023-12-12 21:02:16 +0100

    Minor Pilot.java changes

--- src/model/classes/people/Pilot.java
+++ src/model/classes/people/Pilot.java
@@ -1,9 +1,22 @@
 package model.classes.people;
 
 public class Pilot extends Person {
+    boolean isCaptain;
+    double hoursFlown;
 
-    public Pilot(String pesel, String name, String surname) {
+
+    public Pilot(String pesel, String name, String surname, boolean isCaptain, double hoursFlown) {
         super(pesel, name, surname);
+        this.hoursFlown = hoursFlown;
+        this.isCaptain = isCaptain;
     }
 
+
+    public boolean isCaptain() {return isCaptain;}
+
+    public void setCaptain(boolean captain) {isCaptain = captain;}
+
+    public double getHoursFlown() {return hoursFlown;}
+
+    public void setHoursFlown(double hoursFlown) {this.hoursFlown = hoursFlown;}
 }
