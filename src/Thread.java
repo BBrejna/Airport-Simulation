62bfd42daab53b81d26bcf0fb7d8f578445e7000
BBrejna <bartosz.brejna@gmail.com>
2023-12-16 14:32:19 +0100

    Using Admin as singleton

--- src/Thread.java
+++ src/Thread.java
@@ -7,10 +7,9 @@ public class Thread {
 
     public static void main(String[] args) {
         System.out.println("Bajo jajo 2137");
-        Admin admin = new Admin("213769696", "Gal", "Anonim");
         SalesMan ss = new SalesMan("213769696", "Gal", "Anonim");
         Listener listener = new Listener();
-        Simulation sim = new Simulation("Symulacja", admin, ss, listener);
+        Simulation sim = new Simulation("Symulacja", Admin.getInstance(), ss, listener);
         sim.start();
     }
 
