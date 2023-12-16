5830a48916f0039e2a31f2a6941b3d17dc13ceb4
Dominik Gabis <dominikgabis04@gmail.com>
2023-12-16 22:12:49 +0100

    reformatted salesman + changed addPassenger to boolean

--- src/model/Salesman.java
+++ src/model/Salesman.java
@@ -11,14 +11,15 @@ public class Salesman extends Person {
     //ARIBUTES
     private double cashbox;
     private ArrayList<Passenger> passengers;
-    private static final Salesman instance = new Salesman("11111111111", "Pan", "Ekspedient",0,new ArrayList<>());
+    private static final Salesman instance = new Salesman("11111111111", "Pan", "Ekspedient", 0, new ArrayList<>());
 
     //CONSTRUCTORS
-    public Salesman(String pesel, String name, String surname,double cashbox,ArrayList<Passenger> passengers) {
+    public Salesman(String pesel, String name, String surname, double cashbox, ArrayList<Passenger> passengers) {
         super(pesel, name, surname);
-        this.cashbox=cashbox;
-        this.passengers=passengers;
+        this.cashbox = cashbox;
+        this.passengers = passengers;
     }
+
     public static Salesman getInstance() {
         return instance;
     }
@@ -27,75 +28,87 @@ public class Salesman extends Person {
     public double getCashbox() {
         return cashbox;
     }
+
     public void setCashbox(double cashbox) {
         this.cashbox = cashbox;
     }
+
     public ArrayList<Passenger> getPassengers() {
         return passengers;
     }
+
     public void setPassengers(ArrayList<Passenger> passengers) {
         this.passengers = passengers;
     }
 
     //METHODS
-    public void addPassenger(Passenger passenger){
+    public boolean addPassenger(Passenger passenger) {
         //finding a flight for the passenger
         Flight flight = findFlight(passenger);
+        if (flight == null) {
+            return false;
+        }
         //checking passenger's passport
-        if(!passenger.isPersonalInfo()){
+        if (!passenger.isPersonalInfo()) {
             System.out.println("Incorrect personal info, passenger not served");
-            return;
+            return false;
         }
         //weighing the luggage
-        double multiplier=1;
-        if(luggageOverweight(passenger,flight))multiplier=1.2;
+        double multiplier = 1;
+        if (luggageOverweight(passenger, flight)) multiplier = 1.2;
         //creating an array of available flight classes
-        boolean[] flightClassAvailability=new boolean[flight.getNumOfOccupiedSeats().length];
+        boolean[] flightClassAvailability = new boolean[flight.getNumOfOccupiedSeats().length];
         for (int i = 0; i < flightClassAvailability.length; i++) {
-            //todo update this when airplane gets updated seats
-            //if(flight.getNumOfOccupiedSeats()[i]<flight.getAirplane().getNumberOfSeats()[i])flightClassAvailability[i]=true;
-            if(flight.getNumOfOccupiedSeats()[i]<flight.getAirplane().getNumberOfSeats())flightClassAvailability[i]=true;
-            else flightClassAvailability[i]=false;
+            if (flight.getNumOfOccupiedSeats()[i] < flight.getAirplane().getNumberOfSeatsClasses()[i]) {
+                flightClassAvailability[i] = true;
+            } else flightClassAvailability[i] = false;
         }
         //asking passenger to choose a flight class
-        int flightClass=passenger.chooseFlightClass(flight.getTicketPrice(),flightClassAvailability);
+        int flightClass = passenger.chooseFlightClass(flight.getTicketPrice(), flightClassAvailability);
         //adding passenger to database
-        passenger.setTicket(new Ticket(flight.getFlightNumber(),flightClass));
+        passenger.setTicket(new Ticket(flight.getFlightNumber(), flightClass));
         passengers.add(passenger);
         //changing flight info
-        int[] tmp=flight.getNumOfOccupiedSeats();
+        int[] tmp = flight.getNumOfOccupiedSeats();
         tmp[flightClass]++;
         flight.setNumOfOccupiedSeats(tmp);
         //adding the money to the cashbox
-        cashbox+=flight.getTicketPrice()[flightClass]*multiplier;
+        cashbox += flight.getTicketPrice()[flightClass] * multiplier;
+        return true;
     }
-    public void changeData(Passenger passenger){
-        cashbox+=50;
+
+    public void changeData(Passenger passenger) {
+        cashbox += 50;
     }
-    public void returnTicket(Passenger passenger){
+
+    public void returnTicket(Passenger passenger) {
         Ticket ticket = passenger.getTicket();
-        ArrayList<Flight> flights=getFlights();
-        Flight flight=flights.get(0);
+        ArrayList<Flight> flights = getFlights();
+        Flight flight = flights.get(0);
         for (int i = 0; i < flights.size(); i++) {
-            if(flights.get(i).getFlightNumber()==ticket.getFlightNumber()){
-                flight=flights.get(i);
+            if (flights.get(i).getFlightNumber() == ticket.getFlightNumber()) {
+                flight = flights.get(i);
                 break;
             }
         }
         passengers.remove(passenger);
         passenger.setTicket(null);
-        cashbox-=0.5*flight.getTicketPrice()[ticket.getFlightClass()];
+        cashbox -= 0.5 * flight.getTicketPrice()[ticket.getFlightClass()];
     }
-    public Flight findFlight(Passenger passenger){
-        ArrayList<Flight> list=searchFlights(passenger.getDestinationCity());
+
+    public Flight findFlight(Passenger passenger) {
+        ArrayList<Flight> list = searchFlights(passenger.getDestinationCity());
+        if (list.size() == 0)
+            return null;
         return passenger.chooseFlight(list);
     }
-    public ArrayList<Flight> searchFlights(String city){
+
+    public ArrayList<Flight> searchFlights(String city) {
         ArrayList<Flight> list = getFlights();
         int size = list.size();
         for (int i = 0; i < size; i++) {
-            Flight flight=list.get(i);
-            if(flight.isArrival()||flight.isFull()||flight.getDestinationPoint()!=city){
+            Flight flight = list.get(i);
+            if (flight.isArrival() || flight.isFull() || flight.getDestinationPoint() != city) {
                 size--;
                 list.remove(flight);
                 i--;
@@ -104,25 +117,27 @@ public class Salesman extends Person {
         }
         return list;
     }
-    public ArrayList<Flight> getFlights(){
-        Admin admin= Admin.getInstance();
+
+    public ArrayList<Flight> getFlights() {
+        Admin admin = Admin.getInstance();
         ArrayList<Flight> list = admin.getFlights();
         return list;
     }
-    public boolean luggageOverweight(Passenger passenger, Flight flight){
-        int weight= passenger.getLuggageWeight();
-        //todo update this when airplane gets its atribute
-        //int allowed=flight.getAirplane().getMaxFreeLuggageWeight();
-        int allowed=100;
-        if(weight>allowed)return true;
+
+    public boolean luggageOverweight(Passenger passenger, Flight flight) {
+        int weight = passenger.getLuggageWeight();
+        int allowed = flight.getAirplane().getMaxFreeLuggageWeight();
+        if (weight > allowed) return true;
         return false;
     }
-    public void announceLastCall(Flight flight){
-        System.out.println("Attention, flight no. "+flight.getFlightNumber()+" flying to "+ flight.getDestinationPoint()+
+
+    public void announceLastCall(Flight flight) {
+        System.out.println("Attention, flight no. " + flight.getFlightNumber() + " flying to " + flight.getDestinationPoint() +
                 " is going to take off in 15 minutes");
     }
-    public void announceDelay(Flight flight){
-        System.out.println("Attention, flight no. "+flight.getFlightNumber()+" flying to "+ flight.getDestinationPoint()+
-                " is going to take off with a delay of "+flight.getDelayMinutes()+" due to bad weather conditions");
+
+    public void announceDelay(Flight flight) {
+        System.out.println("Attention, flight no. " + flight.getFlightNumber() + " flying to " + flight.getDestinationPoint() +
+                " is going to take off with a delay of " + flight.getDelayMinutes() + " due to bad weather conditions");
     }
 }
