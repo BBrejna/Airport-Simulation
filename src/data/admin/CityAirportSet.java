7b8b04812c3afca59afdc3c302bce0e8f1b20caa
Kuba280625 <280625@student.pwr.edu.pl>
2023-12-16 21:47:09 +0100

    cities and airport

---
+++ src/data/admin/CityAirportSet.java
@@ -0,0 +1,39 @@
+package data.admin;
+
+//import model.classes.admin.;
+//import model.classes.admin.; <TUTAJ TRZEBA DODAC CO IMPORTUJEMY
+
+public class CityAirportSet {
+
+//    public static final CityAirport[] CITIES_AIRPORTS = {
+//            new CityAirport("New York", "John F. Kennedy International Airport"),
+//            new CityAirport("Los Angeles", "Los Angeles International Airport"),
+//            new CityAirport("Chicago", "O'Hare International Airport"),
+//            new CityAirport("London", "Heathrow Airport"),
+//            new CityAirport("Tokyo", "Haneda Airport"),
+//            new CityAirport("Paris", "Charles de Gaulle Airport"),
+//            new CityAirport("Beijing", "Beijing Capital International Airport"),
+//            new CityAirport("Dubai", "Dubai International Airport"),
+//            new CityAirport("Atlanta", "Hartsfield-Jackson Atlanta International Airport"),
+//            new CityAirport("Shanghai", "Shanghai Pudong International Airport"),
+//            new CityAirport("Hong Kong", "Hong Kong International Airport"),
+//            new CityAirport("Dallas", "Dallas/Fort Worth International Airport"),
+//            new CityAirport("Amsterdam", "Amsterdam Airport Schiphol"),
+//            new CityAirport("Frankfurt", "Frankfurt Airport"),
+//            new CityAirport("Istanbul", "Istanbul Airport"),
+//            new CityAirport("Denver", "Denver International Airport"),
+//            new CityAirport("Singapore", "Singapore Changi Airport"),
+//            new CityAirport("Seoul", "Incheon International Airport"),
+//            new CityAirport("Las Vegas", "McCarran International Airport"),
+//            new CityAirport("San Francisco", "San Francisco International Airport"),
+//            new CityAirport("Munich", "Munich Airport"),
+//            new CityAirport("Madrid", "Adolfo Suárez Madrid–Barajas Airport"),
+//            new CityAirport("Toronto", "Toronto Pearson International Airport"),
+//            new CityAirport("Sydney", "Sydney Airport"),
+//            new CityAirport("Rome", "Leonardo da Vinci International Airport"),
+//            new CityAirport("Miami", "Miami International Airport"),
+//            new CityAirport("Zurich", "Zurich Airport"),
+//            new CityAirport("Osaka", "Kansai International Airport"),
+//            new CityAirport("Bangkok", "Suvarnabhumi Airport"),
+//            new CityAirport("Moscow", "Sheremetyevo International Airport")    };
+}
