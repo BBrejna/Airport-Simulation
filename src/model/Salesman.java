92bb4815b3f99a7ebc70a4384635143e7cc41f97
Dominik Gabis <dominikgabis04@gmail.com>
2023-12-16 17:54:03 +0100

    Ticket implementation

--- src/model/Salesman.java
+++ src/model/Salesman.java
@@ -2,10 +2,10 @@ package model;
 
 import model.classes.people.Person;
 
-public class SalesMan extends Person {
+public class Salesman extends Person {
 
     /** 2137 */
-    public SalesMan(String pesel, String name, String surname) {
+    public Salesman(String pesel, String name, String surname) {
         super(pesel, name, surname);
     }
 
