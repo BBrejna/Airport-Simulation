74b262ea4664f1b53e9c81628300919f6d9c63fb
Dominik Gabis <dominikgabis04@gmail.com>
2023-12-16 17:53:20 +0100

    Ticket implementation

---
+++ src/model/Salesman.java
@@ -0,0 +1,12 @@
+package model;
+
+import model.classes.people.Person;
+
+public class SalesMan extends Person {
+
+    /** 2137 */
+    public SalesMan(String pesel, String name, String surname) {
+        super(pesel, name, surname);
+    }
+
+}
