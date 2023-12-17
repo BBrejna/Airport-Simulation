742f5eda40c1c222cdaca40d370bc81d95013492
BBrejna <bartosz.brejna@gmail.com>
2023-12-17 16:31:58 +0100

    Updated run method in Simulation, added description in Listener from observator pattern example

--- src/model/classes/simulation/Listener.java
+++ src/model/classes/simulation/Listener.java
@@ -5,6 +5,6 @@ import model.classes.Observer;
 public class Listener implements Observer<Weather> {
     @Override
     public void calculateDelayProbability(Weather weather) {
-        System.out.println(weather.getTemperature());
+        System.out.println("Listener: current temperature = "+weather.getTemperature()+"!");
     }
 }
